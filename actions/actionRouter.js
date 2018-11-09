3e883eb2eb591f79f29a515b638b3258f13b3749
DaftBeowulf <ajmboosh@gmail.com>
2018-11-09 11:51:05 -0600

    MVP complete, all CRUD operations and endpoints written for projects and actions, as well as retrieving all listed actions for a given project

---
+++ actions/actionRouter.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const actionDb  =require('../data/helpers/actionModel');
+const projectDb = require('../data/helpers/projectModel');
+const router=express.Router();
+
+router.use(express.json());
+
+//GET /api/actions/
+router.get('/', (req, res)=>{
+    actionDb.get()
+    .then(actions=>{res.status(200)
+    .json(actions)
+    })
+    .catch(error=>{res.status(500)
+    .json({error: "The actions could not be retrieved"})
+    })
+})
+
+//GET by id /api/actions/:id
+router.get('/:id', (req, res) => {
+    
+    const {id} = req.params;
+    
+    actionDb.get(id)
+    .then(action => {
+        if (action) {
+        res.status(200)
+        .json(action);
+        } else {
+            res.status({message: "The action with the specified ID does not exist."})
+        }
+    })
+    .catch(error => {res.status(500)
+        .json({message: "The action info could not be retrieved."})
+    })
+})
+
+//POST /api/actions/
+router.post('/', (req, res) => {
+    const {project_id, description, notes} = req.body;
+    
+    if (!project_id || !description || !notes) {
+    res.status(400)
+    .json({message: "Please provide the notes and description for the new action, as well as the ID of the associated project."})
+    } else {
+
+        projectDb.get(project_id)
+        .then(project=>{
+            if(project) {
+                try {
+                    const actionInfo = req.body;
+                    actionDb.insert(actionInfo);
+                    res.status(201).json(req.body);
+                    } catch (error) {
+                    res.status(500).json({error: "An error occurred while saving this action."})
+                }
+            } else {
+                res.status(404).json({message: "The project with that ID does not exist."})
+            }
+        })
+        .catch(error=>{res.status(404).json({message: "The project with that ID does not exist."})})
+
+    }
+})
+
+//UPDATE /api/actions/:id
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    if (!changes.project_id || !changes.description || !changes.notes) {res.status(400)
+        .json({error: "Please provide the updated action's name, description, and the ID of the associated project."})
+    } else {
+        actionDb.update(id, changes)
+        .then(action => {
+            if (action) {
+            res.status(200)
+            .json(action);
+            } else {
+                res.status(404)
+                .json({message: "The action with the specified ID does not exist."})
+            }
+        })
+        .catch(error => {
+            res.status(500)
+            .json({error: "The action info could not be modified."})
+        })
+    }
+})
+
+//DELETE /api/actions/:id
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    
+    actionDb.remove(id)
+    .then(count=>{
+        if (count) {
+        res.status(200)
+        .json(count);
+        } else {
+            res.status(404)
+            .json({message: "The action with the specififed ID does not exist."})
+        }
+    })
+    .catch(error=>{
+        res.status(500)
+        .json({error: "The action could not be removed."})
+    })
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express=require('express');
 
 const projectRouter = require('./projects/projectRouter');
-// const actionRouter = require('../actions/actionRouter');
+const actionRouter = require('./actions/actionRouter');
 
 const server=express();
 
 server.use('/api/projects', projectRouter);
 
+server.use('/api/actions', actionRouter);
 
 const port = 7000;
 server.listen(port, ()=>console.log(`\nAPI running on port ${port}`))
