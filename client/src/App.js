b3cd295a29e2227716c27a77c5769fd53f537363
DaftBeowulf <ajmboosh@gmail.com>
2018-11-09 12:11:02 -0600

    stretch begun: made react app named client, wrote code to display list of projects on local server. currently experiencing bug getting it to display, working on it

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      projects:[]
+    }
+  }
+
+
+fetchProjects = () => {
+  axios.get('http://localhost:7000/api/projects')
+  .then(res=>this.setState({projects:res.data}))
+  .catch(err=>console.log(err, "We failed to retrieve the list of projects."))
+}
+
+componentDidMount() {
+  this.fetchProjects;
+}
+
+  render() {
+if (!this.state.projects.length) {
+  return <h1>Retrieving projects...</h1>
+} else {
+
+    return (
+      <div className="App">
+ <div className="project-list-wrapper">
+ {this.state.projects.map(project=>{
+   return (
+     <div key={project.id} className="project">
+     <h1 className="project-name">{project.name}</h1>
+     <h3 className="project-description">{project.description}</h3>
+     <h5 className="project-completed">{project.completed}</h5>
+     </div>
+   )
+ })}
+ </div>
+      </div>
+    );
+  }
+}
+}
+
+export default App;
